eb0535096abf6c4eb5ba1f4dab869c588ebf02b8
bekusib <jeenhyung@hanmail.net>
2020-07-30 16:48:00 +0900

    add script.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
-const extractFrames = require('ffmpeg-extract-frames')
+const extractFrames = require('ffmpeg-extract-frames');
 const getVideoDuration = require('node-video-duration');
+// const { getVideoDurationInSeconds } = require('get-video-duration');
 
+const videoConfig = require('./video_config.json');
 
 
 const argv = (() => {
@@ -46,37 +48,69 @@ if (!fs.existsSync(output_dir)){
 // console.log(output_dir);
 
 
+const start_time = argv.starttime || undefined;
+const end_time = argv.endtime || undefined;
 
+
+
+// const timeCalc = () => {
+//   console.log(videoConfig[arSplitFile[0]].start_time);
+
+//   const splitStartTime = videoConfig[arSplitFile[0]].start_time.split(':');
+//   console.log(parseInt(splitStartTime[0] * 36000));
+//   console.log(parseInt(splitStartTime[1] * 6000));
+//   console.log(parseInt(splitStartTime[2] * 1000));
+//   const start_pos = parseInt((splitStartTime[0] * 36000) + (splitStartTime[1] * 6000) + (splitStartTime[2] * 1000));
+//   const splitEndTime = videoConfig[arSplitFile[0]].end_time.split(':');
+//   console.log(parseInt(splitEndTime[0] * 36000));
+//   console.log(parseInt(splitEndTime[1] * 6000));
+//   console.log(parseInt(splitEndTime[2] * 1000));
+//   const end_pos = parseInt((splitEndTime[0] * 36000) + (splitEndTime[1] * 6000) + (splitEndTime[2] * 1000));
+
+
+//   console.log(`start_pos:${start_pos}`);
+//   console.log(`end_pos:${end_pos}`);
+
+//   return {"start_pos": start_pos, "end_pos": end_pos}
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+//
 // 추출
+//
+///////////////////////////////////////////////////////////////////////////////////
 const extractor = (files = [], frame = 1000, output_dir) => {
   console.log(`files: ${JSON.stringify(files)}`);
   console.log(`frame: ${frame}`);
 
   files.forEach(async file => {
-    const input = dir + '/' + file
+    const input = dir + '/' + file;
     const arSplitFile = file.split(".");
 
     console.log(`input: ${input}`);
+    console.log(`arSplitFile: ${arSplitFile}`);
 
     const duration = await getVideoDuration(input).then((duration) => {
       // console.log('Asset duration: ' + duration);
       return duration * 1000;
     });
 
-    // console.log(`duration:${duration}`);
+    console.log(`duration:${duration}`);
 
 
     const size = parseInt(duration / frame + 1);
-    const offsets = Array.from(Array(size), (_, i) => i * frame)
+    const offsets = Array.from(Array(size), (_, i) => i * frame);
 
+    console.log('size:' + JSON.stringify(size));
     // console.log(JSON.stringify(offsets));
 
-    // 이미지 추출
-    await extractFrames({
-      input,
-      output: output_dir + arSplitFile[0] + '/' + arSplitFile[0] + '_frame-%i.jpg',
-      offsets
-    })
+    // // 이미지 추출
+    // await extractFrames({
+    //   input,
+    //   output: output_dir + arSplitFile[0] + '/' + arSplitFile[0] + '_frame-%i.jpg',
+    //   offsets
+    // });
   });
   
 }
---
+++ script.js
@@ -0,0 +1,75 @@
+// ffmpeg -i videos/youtube/1/videoplayback.mp4 -r 5 -f image2 images/youtube/videoplayback/frame_%d.jpg
+const fs = require('fs');
+var shell = require('shelljs');
+
+if (!shell.which('ffmpeg')) {
+  shell.echo('Sorry, this script requires ffmpeg');
+  shell.exit(1);
+}
+
+const argv = (() => {
+  const arguments = {};
+  process.argv.slice(2).map( (element) => {
+      const matches = element.match( '--([a-zA-Z0-9]+)=(.*)');
+      if ( matches ){
+          arguments[matches[1]] = matches[2]
+              .replace(/^['"]/, '').replace(/['"]$/, '');
+      } else if (element.match( '--help') || element.match( '-h')) {
+        console.log(`Flag shorthand -h has been deprecated, please use --help\n`);
+        console.log(`Usage:	node index.js [OPTIONS]\n`);
+        console.log(`Options:\n`);
+        console.log(`\t--frame=1000\t초당 프레임 개수(default: 1)`);
+        console.log(`\t--idir=videos/\t동영상 디렉토리(default: videos/)`);
+        console.log(`\t--odir=images/\t이미지 디렉토리(default: images/)`);
+        process.exit(0);
+      }
+  });
+  return arguments;
+})();
+
+// console.log(argv)
+
+
+const frame = parseInt(argv.frame) || 1;
+// console.log(frame)
+if (typeof frame !== "number" ) {
+  return;
+}
+
+// console.log(argv.idir)
+const i_dir = argv.idir || 'videos/';
+const files =  fs.readdirSync(i_dir); // 디렉토리를 읽어온다
+if(files.length === 0 ) return;
+// console.log(files);
+
+const o_dir = argv.odir === undefined ? 'images/' : argv.odir;
+if (!fs.existsSync(o_dir)){
+  fs.mkdirSync(o_dir);
+}
+// console.log(o_dir);
+
+
+
+files.forEach(file => {
+  const arSplitFile = file.split(".");
+  const input_path = i_dir + file;
+  const out_path = o_dir + arSplitFile[0] + "/frame_%d.jpg";
+  
+  console.log(`input_path: ${input_path}`);
+  console.log(`out_path: ${out_path}`);
+
+  if (!fs.existsSync(o_dir + arSplitFile[0])){
+    fs.mkdirSync(o_dir + arSplitFile[0]);
+  }
+
+  const cmd = `ffmpeg -i ${input_path} -r ${frame} -f image2 ${out_path}`;
+  console.log(`cmd: ${cmd}`);
+
+  var child = shell.exec(cmd, {async:true});
+  child.stdout.on('data', function(data) {
+    /* ... do something with data ... */
+    console.log(data);
+  });
+
+});
+
